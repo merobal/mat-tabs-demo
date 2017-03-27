6224903c6cd4294d49cd5f649d9f38844feb969e
merobal <facebook@merobal.hu>
2017-03-27 16:29:27 +0200

    Add mat-tab-group for demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { MaterialModule, NoConflictStyleCompatibilityMode } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -11,8 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    NoConflictStyleCompatibilityMode
   ],
   providers: [],
   bootstrap: [AppComponent]
